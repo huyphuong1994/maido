27b186812ff51cadb1e6ea24c57ea251299deae9
huyphuong94 <phuongdh@caro.vn>
2023-10-27 02:22:48 +0700

    fix bug:

--- src/components/Container1.vue
+++ src/components/Container1.vue
@@ -1,45 +1,45 @@
 <template>
   <div
-    class="w-[1200px] flex flex-row items-center justify-between relative text-left text-53xl text-darkslategray-200 font-butler"
-    data-scroll-to="container1"
+      class="w-[1200px] flex flex-row items-center justify-between relative text-left text-53xl text-darkslategray-200 font-butler"
+      data-scroll-to="container1"
   >
     <div class="flex flex-col items-start justify-start gap-[32px] z-[0]">
       <div class="flex flex-col items-start justify-start">
-        <div class="self-stretch relative leading-[120%]">Naniel & Dessica</div>
+        <div class="self-stretch relative leading-[120%]">Ngọc Độ & Ngọc Mai</div>
       </div>
       <div
-        class="self-stretch flex flex-row items-center justify-start gap-[56px] text-center text-sienna"
+          class="self-stretch flex flex-row items-center justify-start gap-[56px] text-center text-sienna"
       >
         <div class="flex flex-col items-center justify-start">
           <div class="relative leading-[120%] uppercase inline-block w-20">
-            30
+            {{ days }}
           </div>
           <div
-            class="relative text-xl leading-[150%] uppercase font-inter text-dimgray"
+              class="relative text-xl leading-[150%] uppercase font-inter text-dimgray"
           >
             Days
           </div>
         </div>
         <div class="flex flex-col items-center justify-start">
-          <div class="relative leading-[120%] uppercase">14</div>
+          <div class="relative leading-[120%] uppercase">{{hours}}</div>
           <div
-            class="relative text-xl leading-[150%] uppercase font-inter text-dimgray"
+              class="relative text-xl leading-[150%] uppercase font-inter text-dimgray"
           >
             Hours
           </div>
         </div>
         <div class="flex flex-col items-center justify-start">
-          <div class="relative leading-[120%] uppercase">03</div>
+          <div class="relative leading-[120%] uppercase">{{minutes}}</div>
           <div
-            class="relative text-xl leading-[150%] uppercase font-inter text-dimgray"
+              class="relative text-xl leading-[150%] uppercase font-inter text-dimgray"
           >
             Mins
           </div>
         </div>
         <div class="flex flex-col items-center justify-start">
-          <div class="relative leading-[120%] uppercase">34</div>
+          <div class="relative leading-[120%] uppercase">{{seconds}}</div>
           <div
-            class="relative text-xl leading-[150%] uppercase font-inter text-dimgray"
+              class="relative text-xl leading-[150%] uppercase font-inter text-dimgray"
           >
             Secs
           </div>
@@ -48,43 +48,52 @@
     </div>
     <div class="relative w-[379px] h-[532px] z-[1]">
       <img
-        class="absolute top-[0px] left-[0px] rounded-461xl w-[379px] h-[532px] object-cover [&.animate]:animate-[1s_ease_0s_1_normal_forwards_fade-in-bottom] opacity-[0]"
-        alt=""
-        src="/image@2x.png"
-        data-animate-on-scroll
+          class="absolute top-[0px] left-[0px] rounded-461xl w-[379px] h-[532px] object-cover [&.animate]:animate-[1s_ease_0s_1_normal_forwards_fade-in-bottom] opacity-[0]"
+          alt=""
+          src="/image@2x.png"
+          data-animate-on-scroll
       />
     </div>
     <div
-      class="absolute my-0 mx-[!important] bottom-[24px] left-[719px] w-40 h-40 [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] z-[2]"
-      data-animate-on-scroll
+        class="absolute my-0 mx-[!important] bottom-[24px] left-[719px] w-40 h-40 [&.animate]:animate-[1s_ease_0s_1_normal_forwards_scale-up] opacity-[1] z-[2]"
+        data-animate-on-scroll
     >
       <img
-        class="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/group.svg"
+          class="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/group.svg"
       />
       <img
-        class="absolute h-[30.38%] w-[30.38%] top-[34.94%] right-[34.69%] bottom-[34.69%] left-[34.94%] max-w-full overflow-hidden max-h-full"
-        alt=""
-        src="/arrow-downward-fill0-wght400-grad0-opsz24-1.svg"
+          class="absolute h-[30.38%] w-[30.38%] top-[34.94%] right-[34.69%] bottom-[34.69%] left-[34.94%] max-w-full overflow-hidden max-h-full"
+          alt=""
+          src="/arrow-downward-fill0-wght400-grad0-opsz24-1.svg"
       />
     </div>
   </div>
 </template>
 <script>
-  import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 
-  export default defineComponent({
-    name: "Container1",
-    data() {
-      return { observer: null };
-    },
+export default defineComponent({
+  name: "Container1",
+  data() {
+    return {
+      observer: null,
+      targetDate: new Date('2023-11-21T00:00:00'),
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    };
+  },
 
-    mounted() {
-      const scrollAnimElements = document.querySelectorAll(
+  mounted() {
+    setInterval(this.timeRemaining, 1000)
+
+    const scrollAnimElements = document.querySelectorAll(
         "[data-animate-on-scroll]"
-      );
-      this.observer = new IntersectionObserver(
+    );
+    this.observer = new IntersectionObserver(
         (entries) => {
           for (const entry of entries) {
             if (entry.isIntersecting || entry.intersectionRatio > 0) {
@@ -97,14 +106,37 @@
         {
           threshold: 0.15,
         }
-      );
+    );
 
-      for (let i = 0; i < scrollAnimElements.length; i++) {
-        this.observer.observe(scrollAnimElements[i]);
+    for (let i = 0; i < scrollAnimElements.length; i++) {
+      this.observer.observe(scrollAnimElements[i]);
+    }
+  },
+
+  methods: {
+    timeRemaining() {
+      var currentTime = new Date();
+      var timeDifference = this.targetDate - currentTime;
+
+      if (timeDifference <= 0) {
+        return {
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0
+        };
       }
-    },
-    beforeUnmount() {
-      this.observer.disconnect();
-    },
-  });
+
+
+      this.days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    }
+  },
+
+  beforeUnmount() {
+    this.observer.disconnect();
+  },
+});
 </script>
--- src/components/Section2.vue
+++ src/components/Section2.vue
@@ -1,40 +1,41 @@
 <template>
   <div
-    class="bg-white overflow-hidden flex flex-col items-start justify-start text-left text-53xl text-white font-butler"
+      class="bg-white overflow-hidden flex flex-col items-start justify-start text-left text-53xl text-white font-butler"
   >
     <div
-      class="w-full flex flex-col items-start justify-start relative gap-[8px]"
+        class="w-full flex flex-col items-start justify-start relative gap-[8px]"
     >
       <div class="relative w-full h-[968px] z-[0]">
         <img
-          class="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
-          alt=""
-          src="/image-11@2x.png"
-          data-scroll-to="image11"
+            class="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full object-cover"
+            alt=""
+            src="/image-11@2x.png"
+            data-scroll-to="image11"
         />
       </div>
       <div
-        class="my-0 mx-[!important] absolute top-[154px] left-[120px] flex flex-col items-start justify-start gap-[96px] z-[1]"
+          class="my-0 mx-[!important] absolute top-[154px] left-[120px] flex flex-col items-start justify-start gap-[96px] z-[1]"
       >
         <div class="relative leading-[120%]">Time and place</div>
         <div
-          class="self-stretch flex flex-col items-start justify-start gap-[48px] text-5xl font-inter"
+            class="self-stretch flex flex-col items-start justify-start gap-[48px] text-5xl font-inter"
         >
           <div
-            class="self-stretch flex flex-col items-start justify-start gap-[16px]"
+              class="self-stretch flex flex-col items-start justify-start gap-[16px]"
           >
             <div class="self-stretch relative leading-[150%]">
-              <p class="m-0">Monday, 25 Sep, 2023</p>
-              <p class="m-0">1:00 PM – 4:30 PM</p>
+              <p class="m-0">Thứ 3, 21-11-2023</p>
+              <p class="m-0">16:00 PM – 17:30 PM</p>
             </div>
             <div class="self-stretch relative leading-[150%]">
-              Estern Star Plaza, Road 123, USA
+              Số 6, Đường Hoàng Diệu, Phường Phương Lâm, TP.Hòa Bình, T.Hòa Bình
             </div>
           </div>
           <div
-            class="bg-white w-[221px] overflow-hidden flex flex-row items-center justify-center p-4 box-border text-base text-gray-100"
+              class="bg-white w-[221px] overflow-hidden flex flex-row items-center justify-center p-4 box-border text-base text-gray-100"
           >
-            <div class="relative tracking-[1px] leading-[150%]">View map</div>
+            <a target="_blank" href="https://www.google.com/maps/place/Ho%C3%A0ng+Di%E1%BB%87u,+Ph%C6%B0%C6%A1ng+L%C3%A2m,+Tp.+H%C3%B2a+B%C3%ACnh,+H%C3%B2a+B%C3%ACnh,+Vietnam/@20.8173692,105.3319223,17z/data=!3m1!4b1!4m10!1m2!2m1!1zU-G7kSA2LCDEkMaw4budbmcgSG_DoG5nIERp4buHdSwgUGjGsOG7nW5nIFBoxrDGoW5nIEzDom0sIFRQLkjDsmEgQsOsbmgsIFQuSMOyYSBCw6xuaA!3m6!1s0x31346b7356f4d43d:0xc073d96e522f8039!8m2!3d20.817365!4d105.3360182!15sClVT4buRIDYsIMSQxrDhu51uZyBIb8OgbmcgRGnhu4d1LCBQaMaw4budbmcgUGjGsMahbmcgTMOibSwgVFAuSMOyYSBCw6xuaCwgVC5Iw7JhIELDrG5okgEFcm91dGXgAQA!16s%2Fg%2F11bwfy8lp9?entry=ttu"
+               class="relative tracking-[1px] leading-[150%]">View map</a>
           </div>
         </div>
       </div>
@@ -42,9 +43,9 @@
   </div>
 </template>
 <script>
-  import { defineComponent } from "vue";
+import {defineComponent} from "vue";
 
-  export default defineComponent({
-    name: "Section2",
-  });
+export default defineComponent({
+  name: "Section2",
+});
 </script>
